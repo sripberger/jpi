2b24e8f38cf125f0e0fdb410277c6e5860efb7c7
Steve Ripberger <sripberger@batterii.com>
2019-03-25 14:02:29 -0400

    Begin redesign of RequestHandler

--- test/unit/request-handler.js
+++ test/unit/request-handler.js
@@ -1,31 +1,25 @@
-import { ClientRequest, ServerResponse } from 'http';
 import { MethodManager } from '../../lib/method-manager';
 import { MiddlewareManager } from '../../lib/middleware-manager';
+import { Request } from '../../lib/request';
 import { RequestHandler } from '../../lib/request-handler';
 
 describe('RequestHandler', function() {
-	let request, response, middlewareManager, methodManager, handler;
+	let request, middlewareManager, methodManager, handler;
 
 	beforeEach(function() {
-		request = sinon.createStubInstance(ClientRequest);
-		response = sinon.createStubInstance(ServerResponse);
+		request = sinon.createStubInstance(Request);
 		middlewareManager = new MiddlewareManager();
 		methodManager = new MethodManager();
 
 		handler = new RequestHandler(
 			request,
-			response,
 			middlewareManager,
 			methodManager
 		);
 	});
 
-	it('stores provided client request', function() {
+	it('stores provided request object', function() {
 		expect(handler.request).to.equal(request);
-	});
-
-	it('stores provided server response', function() {
-		expect(handler.response).to.equal(response);
 	});
 
 	it('stores provided middleware manager', function() {
