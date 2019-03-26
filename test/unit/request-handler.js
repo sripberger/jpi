bc2c4dc33a4d72ac8b539b47f892814c923de0b1
Steve Ripberger <sripberger@batterii.com>
2019-03-26 12:58:59 -0400

    Finish implementing RequestHandler#run

--- test/unit/request-handler.js
+++ test/unit/request-handler.js
@@ -1,24 +1,27 @@
+import { ContextManager } from '../../lib/context-manager';
 import { MethodManager } from '../../lib/method-manager';
 import { MiddlewareManager } from '../../lib/middleware-manager';
 import { Request } from '../../lib/request';
 import { RequestHandler } from '../../lib/request-handler';
 
 describe('RequestHandler', function() {
-	let request, middlewareManager, methodManager, handler;
+	let request, middlewareManager, methodManager, httpRequest, handler;
 
 	beforeEach(function() {
-		request = sinon.createStubInstance(Request);
+		request = new Request();
 		middlewareManager = new MiddlewareManager();
 		methodManager = new MethodManager();
+		httpRequest = {};
 
 		handler = new RequestHandler(
 			request,
 			middlewareManager,
-			methodManager
+			methodManager,
+			httpRequest
 		);
 	});
 
-	it('stores provided request object', function() {
+	it('stores provided Request instance', function() {
 		expect(handler.request).to.equal(request);
 	});
 
@@ -30,7 +33,92 @@ describe('RequestHandler', function() {
 		expect(handler.methodManager).to.equal(methodManager);
 	});
 
+	it('stores provided http request', function() {
+		expect(handler.httpRequest).to.equal(httpRequest);
+	});
+
 	describe('#run', function() {
-		it('handles the request');
+		const runResult = { run: 'result' };
+		let contextManager, result;
+
+		beforeEach(async function() {
+			contextManager = sinon.createStubInstance(ContextManager);
+			contextManager.run.resolves(runResult);
+			sinon.stub(request, 'validate');
+			sinon.stub(handler, '_getContextManager').returns(contextManager);
+
+			result = await handler.run();
+		});
+
+		it('validates the request', function() {
+			expect(request.validate).to.be.calledOnce;
+		});
+
+		it('fetches a context manager after validating', function() {
+			expect(handler._getContextManager).to.be.calledOnce;
+			expect(handler._getContextManager).to.be.calledOn(handler);
+			expect(handler._getContextManager)
+				.to.be.calledAfter(request.validate);
+		});
+
+		it('runs the fetched context manager', function() {
+			expect(contextManager.run).to.be.calledOnce;
+			expect(contextManager.run).to.be.calledOn(contextManager);
+		});
+
+		it('resolves with result of context manager run', function() {
+			expect(result).to.equal(runResult);
+		});
+	});
+
+	describe('#_getContextManager', function() {
+		const method = 'some method';
+		const params = { some: 'params' };
+		const headers = { some: 'headers' };
+		const rawHeaders = { some: 'raw headers' };
+		const methodOptions = { method: 'options' };
+		const premethodMiddlewares = [ () => {}, () => {} ];
+		const methodMiddlewares = [ () => {}, () => {} ];
+		const postmethodMiddlewares = [ () => {}, () => {} ];
+		let result;
+
+		beforeEach(function() {
+			request.method = method;
+			request.params = params;
+			httpRequest.headers = headers;
+			httpRequest.rawHeaders = rawHeaders;
+			sinon.stub(middlewareManager, 'premethod')
+				.get(() => premethodMiddlewares);
+			sinon.stub(middlewareManager, 'postmethod')
+				.get(() => postmethodMiddlewares);
+			sinon.stub(methodManager, 'getMethod').returns({
+				middlewares: methodMiddlewares,
+				options: methodOptions,
+			});
+
+			result = handler._getContextManager();
+		});
+
+		it('gets method entry for request.method', function() {
+			expect(methodManager.getMethod).to.be.calledOnce;
+			expect(methodManager.getMethod).to.be.calledOn(methodManager);
+			expect(methodManager.getMethod).to.be.calledWith(method);
+		});
+
+		it('returns a ContextManager with necessary references', function() {
+			expect(result).to.be.an.instanceof(ContextManager);
+			expect(result.context).to.deep.equal({
+				method,
+				methodOptions,
+				params,
+				headers,
+				rawHeaders,
+			});
+			expect(result.middlewares).to.deep.equal({
+				premethod: premethodMiddlewares,
+				method: methodMiddlewares,
+				postmethod: postmethodMiddlewares,
+			});
+		});
 	});
 });
